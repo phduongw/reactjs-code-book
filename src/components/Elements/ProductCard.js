2aa82f67c45008679f3b49aa9306551565a087b3
phduongw <eganpham.99@gmail.com>
2024-11-19 00:49:06 +0700

    Fix typo

--- src/components/Elements/ProductCard.js
+++ src/components/Elements/ProductCard.js
@@ -12,7 +12,7 @@ const ProductCard = ({ product }) => {
         best_seller,
         image_local
     } = product;
-    console.log("Best seller is: ", best_seller)
+
     return (
         <div
             className="m-3 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
